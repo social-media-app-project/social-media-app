0a0b8d783832bb77cf31bb0f90829fddc324d83f
Rajdeep Dhillon <rdhillon1016@gmail.com>
2022-05-29 17:12:33 -0700

    added friends page

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from './NavBar.module.css';
 import NavBarButton from './NavBarButton/NavBarButton';
-import { FaCog, FaHome, FaUserAlt } from 'react-icons/fa';
+import { FaCog, FaHome, FaUserAlt, FaUserFriends } from 'react-icons/fa';
 
 const NavBar = () => {
   const buttons = [
     { to: "/home", label: "Home", icon: <FaHome/> },
     { to: "/home", label: "Profile", icon: <FaUserAlt/> }, // TODO: change route
+    { to: "/home", label: "Friends", icon: <FaUserFriends/>},
     { to: "/home", label: "Settings", icon: <FaCog/> } // TODO: change route
   ];
 
