464cae1beb3029829068ef0fbcea6d9c3f4acb00
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-06-03 11:07:47 -0700

    Fixes #18

--- frontend/src/components/NavBar/NavBarButton/NavBarButton.js
+++ frontend/src/components/NavBar/NavBarButton/NavBarButton.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import styles from './NavBarButton.module.css';
 import { NavLink } from 'react-router-dom';
 
 // Temporary settings stub
 const NavBarButton = props => {
+  const [isMobile, setIsMobile] = useState(false);
+  
+  const resize = () =>{
+    if(window.innerWidth<930){
+      setIsMobile(true);
+    }else{
+      setIsMobile(false);
+    }
+  }
+
+  useEffect(()=>{
+    window.addEventListener('resize', resize)
+
+    return () =>{
+      window.removeEventListener('resize',resize);
+    }
+  })
+
+
   return (
     <NavLink 
       to={props.to} 
@@ -14,7 +33,11 @@ const NavBarButton = props => {
         }
       }}>
         {props.children}
-        {props.label ? <span className={styles['nav-button-label']}>{props.label}</span> : null}
+        {
+          !isMobile &&
+            props.label ? <span className={styles['nav-button-label']}>{props.label}</span> : null
+        }
+
     </NavLink>
   );
 };
