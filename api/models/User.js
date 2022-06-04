8580582ce2a97532fc3d6a4145453cc6d8dbd7e0
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-06-03 18:32:59 -0700

    asdf

--- api/models/User.js
+++ api/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const {DateTime} = require('luxon');
 
 const UserSchema = new Schema({
   first_name:{type:String,required:true, minlength:2},
@@ -7,7 +8,19 @@ const UserSchema = new Schema({
   username:{type:String,required:true, minlength:2},
   password:{type:String,required:true, minlength:2},
   profilePicUrl:{type:String},
-  date_create:{type:Date, required:true,default:Date.now()},
+  date_created:{type:Date, required:true,default:Date.now()},
 })
+
+UserSchema.virtual('full_name')
+  .get(function(){
+    return this.first_name+ " "+this.last_name;
+  })
+
+UserSchema.virtual('formatted_date_created')
+  .get(function(){
+    return DateTime.fromJSDate(this.date_created).toLocaleString(DateTime.DATE_MED)
+
+  })
+
 
 module.exports = mongoose.model('User', UserSchema);
