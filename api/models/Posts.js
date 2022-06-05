f76e311423ab6ac19f0392455bb23fda0e5e0b3f
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-06-05 16:23:00 -0700

    post schema

---
+++ api/models/Posts.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  author :{type:Schema.Types.ObjectId, ref:'Users'},
+  date:{type:Date, required:'true', default:Date.now()},
+  message:{type:String},
+  // image:{type:String}
+})
+
+module.exports = mongoose.model('Posts',PostSchema);
