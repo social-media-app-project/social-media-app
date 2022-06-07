dcc41613a6a74ecd6b1ca337cd3e9f673d105559
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-06-07 10:02:27 -0700

    added auth route

--- api/app.js
+++ api/app.js
@@ -13,6 +13,7 @@ db.on('error',console.error.bind(console,'mongo connection error'))
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var authArouter = require('./routes/auth');
 
 var app = express();
 
@@ -24,5 +25,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth',authArouter);
 
 module.exports = app;
