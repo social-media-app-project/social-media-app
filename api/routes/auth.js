3609e3d7ed408824f77cf8c3dc01e33e70a2e5ac
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-06-07 10:02:52 -0700

    added sign up route

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,4 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const AuthController = require('../controller/AuthController')
 
-router.post('/login',)
+router.post('/signup',AuthController.jwt_signup_post)
+
+module.exports = router;
