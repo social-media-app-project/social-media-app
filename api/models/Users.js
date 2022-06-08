847d9a0fa25d945324fe1431dd464e55c76b1d74
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-06-07 17:08:11 -0700

    asdf

--- api/models/Users.js
+++ api/models/Users.js
@@ -7,6 +7,8 @@ const UserSchema = new Schema({
   last_name:{type:String,required:true, minlength:2},
   username:{type:String,required:true, minlength:2},
   password:{type:String,required:true, minlength:2},
+  email:{type:String,required:true, minlength:5},
+  dob:{type:String},
   profilePicUrl:{type:String},
   date_created:{type:Date, required:true,default:Date.now()},
   friends:[{type:Schema.Types.ObjectId, ref:'Friends'}],
