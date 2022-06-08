b4f693d92d442611316f0b4ec09d3a79e88c0f37
Rajdeep Dhillon <rdhillon1016@gmail.com>
2022-06-08 13:13:07 -0700

    Added additional methods

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -2,16 +2,18 @@ const express = require('express');
 const PostsController = require('../controllers/PostsController');
 
 module.exports = (passport) => {
-    const router = express.Router();
+  const router = express.Router();
 
-    router.get('/posts/feed', passport.authenticate('jwt', { session: false }), PostsController.getFeed);
-    router.get('/posts/:postId', PostsController.getPost)
-    router.get('/posts/:postId/comments', PostsController.getComments);
-    router.get('/posts/:postId/likes', PostsController.getLikes)
-    
-    router.post('/posts', passport.authenticate('jwt', { session: false }), PostsController.post);
-    
-    router.delete('/posts/:postId', passport.authenticate('jwt', { session: false }), PostsController.deletePost);
+  router.get('/posts/feed', passport.authenticate('jwt', { session: false }), PostsController.getFeed);
+  router.get('/posts/:postId', PostsController.getPost)
+  router.get('/posts/:postId/comments', PostsController.getComments);
+  router.get('/posts/:postId/likes', PostsController.getLikes);
 
-    return router;
+  router.post('/posts', passport.authenticate('jwt', { session: false }), PostsController.post);
+  router.post('/posts/:postId/comments', passport.authenticate('jwt', { session: false }), PostsController.commentPost);
+  router.post('/posts/:postId/likes', passport.authenticate('jwt', { session: false }), PostsController.likePost);
+
+  router.delete('/posts/:postId', passport.authenticate('jwt', { session: false }), PostsController.deletePost);
+
+  return router;
 };
