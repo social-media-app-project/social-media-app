059918fc948186ed6109e2586585680b57ff2f3b
Baltej Randhawa <35308786+brandhawa99@users.noreply.github.com>
2022-06-11 14:07:48 -0700

    50 days since (#53)
    * added sign in with google without functionality
    
    * sign in with google and fb button
    
    * days ago

--- frontend/src/components/Post/PostDateLabel/PostDateLabel.js
+++ frontend/src/components/Post/PostDateLabel/PostDateLabel.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import styles from './PostDateLabel.module.css';
-import moment from 'moment';
+import React from "react";
+import styles from "./PostDateLabel.module.css";
+import moment from "moment";
 
-const PostDateLabel = props => {
-    const date = new Date(props.timestamp);
-    const formattedString = moment(date).format("MMMM Do YYYY, h:mm a");
+const PostDateLabel = (props) => {
+  const date = new Date(props.timestamp);
+  const formattedString = moment(date).fromNow();
 
-    return (
-    <span className={styles['date-label']}>{formattedString}</span>
-    )
+  return <span className={styles["date-label"]}>{formattedString}</span>;
 };
 
 export default PostDateLabel;
