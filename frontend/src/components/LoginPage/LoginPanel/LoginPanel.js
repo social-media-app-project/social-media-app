33bbb8db8c6ff41b3ef76faaed558254077f334d
Baltej Randhawa <35308786+brandhawa99@users.noreply.github.com>
2022-06-11 14:07:06 -0700

    7 add sign in with google (#51)
    * added sign in with google without functionality
    
    * sign in with google and fb button
    
    * changed colour (#28)
    
    * images weren't centered
    
    * used husky old version (#26)
    
    * 9 design database (#29)
    
    * friends schema
    
    * post schema
    
    * user schema
    
    * asdf
    
    * image link
    
    * 30 implment auth (#41)
    
    * initial auth commit
    
    * added user signup
    
    * added async and bcryptjs package
    
    * removed  dob cuz its annoying to test with
    
    * added auth route
    
    * removed dob
    
    * added sign up route
    
    * fixed header error
    
    * created jwt sign in
    
    * set up passport jwt strategy
    
    * asdf
    
    * asdf
    
    * asdf
    
    * asdf
    
    * added harsher linting for readability
    
    * asdf
    
    * extracted out cb
    
    * added sign in with google without functionality
    
    * sign in with google and fb button
    
    Co-authored-by: rdhillon1016 <57784318+rdhillon1016@users.noreply.github.com>

--- frontend/src/components/LoginPage/LoginPanel/LoginPanel.js
+++ frontend/src/components/LoginPage/LoginPanel/LoginPanel.js
@@ -1,23 +1,37 @@
 import TextButton from "../../common/form/TextButton/TextButton";
 import LoginForm from "../LoginForm/LoginForm";
 import styles from "./LoginPanel.module.css";
-import React from 'react';
+import React from "react";
+import SignInWithPanel from "../SignInWithPanel/SignInWithPanel";
 
 function LoginPanel() {
-    return <div className={styles["login-panel"]}>
-        <div className={styles["login-heading-container"]}>
-            <h1 className={styles["login-heading"]}>Welcome</h1>
+  return (
+    <div className={styles["login-panel"]}>
+      <div className={styles["login-heading-container"]}>
+        <h1 className={styles["login-heading"]}>Welcome</h1>
+      </div>
+      <div className={styles["inputs-container"]}>
+        <LoginForm />
+        <hr></hr>
+        <SignInWithPanel />
+
+        <div className={styles["password-button-container"]}>
+          <TextButton
+            text="Forgot Password?"
+            classNames={[styles["forgot-password-button"]]}
+            type="button"
+          />
         </div>
-        <div className={styles["inputs-container"]}>
-            <LoginForm/>
-            <div className={styles["password-button-container"]}>
-                <TextButton text="Forgot Password?" classNames={[styles["forgot-password-button"]]} type="button" />
-            </div>
-            <div className={styles["create-button-container"]}>
-                <TextButton text="Create a new account" classNames={[styles["create-account-button"]]} type="button" />
-            </div>
+        <div className={styles["create-button-container"]}>
+          <TextButton
+            text="Create a new account"
+            classNames={[styles["create-account-button"]]}
+            type="button"
+          />
         </div>
-    </div>;
+      </div>
+    </div>
+  );
 }
 
 export default LoginPanel;
