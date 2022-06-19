9a1c91c3837ae003bbddb84cd41c603e5fd72300
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-06-19 14:20:28 -0700

    initial commit

--- frontend/src/components/Friends/Friends.js
+++ frontend/src/components/Friends/Friends.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import styles from './Friends.module.css';
+import React from "react";
+import styles from "./Friends.module.css";
+import Friend from "./Friend/Friend";
 
 const Friends = () => {
-    // Fetch data using route path and params
+  // Fetch data using route path and params
 
-    return (
-    <div>Friends</div>
-    )
+  return (
+    <div className={styles["friends-container"]}>
+      <h1 className={styles["friends-header"]}>Friends</h1>
+      <Friend />
+    </div>
+  );
 };
 
 export default Friends;
