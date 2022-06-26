cd008494cb4822e39a923684d43bc85a9ba52a05
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-06-25 18:47:14 -0700

    initial commit

--- api/models/User.js
+++ api/models/User.js
@@ -8,6 +8,7 @@ const UserSchema = new Schema({
   password: { type: String, required: true, minlength: 2 },
   email: { type: String, required: true, minlength: 5 },
   profilePicUrl: { type: String },
+  facebookID: { type: String },
   date_created: { type: Date, required: true, default: Date.now() },
   friends: [{ type: Schema.Types.ObjectId, ref: 'User' }],
   outgoing_requests: [{ type: Schema.Types.ObjectId, ref: 'User' }],
