7c1e173d7f55893fc505ec9dc7be7a738c0d5aed
rdhillon1016 <57784318+rdhillon1016@users.noreply.github.com>
2022-07-04 15:40:53 -0700

    added a request input (#78)

--- frontend/src/test-data/user-data.js
+++ frontend/src/test-data/user-data.js
@@ -1,6 +1,7 @@
+import profilePicUrl from "./test-images/profilePicTest.jpg";
+
 const user = {
-  profilePicUrl:
-    "https://pbs.twimg.com/profile_images/1343497830099402752/kBLCEgPl_normal.jpg",
+  profilePicUrl,
   username: "meeemeee",
   displayName: "bob bobb",
   bio: "Hi, this is my really cool bio",
