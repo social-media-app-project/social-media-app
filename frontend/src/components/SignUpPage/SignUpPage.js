5c52ceffced9912c0f67ad9f62995d5926c8b7cd
Baltej Randhawa <baltej.randhawa0@gmail.com>
2023-01-08 13:44:23 -0800

    initial commit

--- frontend/src/components/SignUpPage/SignUpPage.js
+++ frontend/src/components/SignUpPage/SignUpPage.js
@@ -1,10 +1,11 @@
 import React from "react";
 import styles from "./SignUpPage.module.css";
+import SignUpPageForm from "./SignUpPageForm/SignUpPageForm";
 
 const SignUpPage = () => {
   return (
     <div className={styles["signup-page"]}>
-      <div>Text</div>
+      <SignUpPageForm />
     </div>
   );
 };
---
+++ frontend/src/components/components/SignUpPage/components/SignUpPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./ComponentsSignUpPage.module.css";
+
+const ComponentsSignUpPage = () => {
+  return <div></div>;
+};
+
+export default ComponentsSignUpPage;
