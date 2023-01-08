0ab344f7e055ee42fedb1956e0d783bd6295616d
Baltej Randhawa <baltej.randhawa0@gmail.com>
2023-01-08 13:35:34 -0800

    initialize signup page

---
+++ frontend/src/components/SignUpPage/SignUpPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./SignUpPage.module.css";
+
+const SignUpPage = () => {
+  return (
+    <div className={styles["signup-page"]}>
+      <div>Text</div>
+    </div>
+  );
+};
+
+export default SignUpPage;
