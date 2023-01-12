35ea2cb62231122d3c03f2af4f3dd96fb07f90c4
Baltej Randhawa <baltej.randhawa0@gmail.com>
2023-01-11 19:40:01 -0800

    added cors

--- api/app.js
+++ api/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 require('dotenv').config();
 const passport = require('passport');
 const JWTStrategy = require('./strategies/jwt');
@@ -14,7 +15,9 @@ app.use(express.json());
 app.use(passport.initialize());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
+app.disable('x-powered-by');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -23,8 +26,16 @@ const postsRouter = require('./routes/posts');
 
 app.use('/', indexRouter);
 app.use('/auth', authArouter);
-app.use('/users', passport.authenticate('jwt', { session: false }), usersRouter);
-app.use('/posts', passport.authenticate('jwt', { session: false }), postsRouter);
+app.use(
+  '/users',
+  passport.authenticate('jwt', { session: false }),
+  usersRouter,
+);
+app.use(
+  '/posts',
+  passport.authenticate('jwt', { session: false }),
+  postsRouter,
+);
 
 app.use((err, req, res, next) => {
   if (res.headersSent) {
