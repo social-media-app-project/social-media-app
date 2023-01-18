9530ae2bf4bf5b63617e54f2f28f63f848d93708
Baltej Randhawa <baltej.randhawa0@gmail.com>
2023-01-17 23:22:59 -0800

    validate token

--- api/middleware/validators/util.js
+++ api/middleware/validators/util.js
@@ -1,4 +1,6 @@
-const { validationResult } = require('express-validator');
+const { validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const User = require("../../models/User");
 
 exports.sendResponseOnError = (req, res, next) => {
   const errors = validationResult(req);
@@ -7,4 +9,18 @@ exports.sendResponseOnError = (req, res, next) => {
   } else {
     next();
   }
+};
+
+exports.validateToken = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+    const decoded = jwt.verify(token, process.env.SECRET);
+    const user = await User.findById(decoded.userid);
+    if (!user) {
+      res.statusCode(401).send({ errors: "Not Authorized" });
+    } else {
+      req.user = user;
+      next();
+    }
+  } catch (error) {}
 };
