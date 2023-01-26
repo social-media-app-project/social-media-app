5016acb6f3990708aed94429823f7560a03b668a
Baltej Randhawa <baltej.randhawa0@gmail.com>
2023-01-25 19:45:16 -0800

    fetch post comments

--- frontend/src/components/Post/CommentsSection/CommentsSection.js
+++ frontend/src/components/Post/CommentsSection/CommentsSection.js
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./CommentsSection.module.css";
-import { comments } from "../../../test-data/post-data.js";
 import CreateComment from "./CreateComment/CreateComment";
 import Comment from "./Comment/Comment";
+import { v4 } from "uuid";
+import { getPostComments } from "../../../services/postService";
 
-const CommentsSection = () => {
+const CommentsSection = ({ postID }) => {
+  const [commentsArr, setComments] = useState([]);
+
+  useEffect(() => {
+    const getFetchComments = async () => {
+      try {
+        const response = await getPostComments(postID);
+        const data = await response.json();
+        if (response.ok) {
+          setComments(data.comments);
+        } else {
+          setComments([]);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getFetchComments();
+  }, []);
+
   return (
     <div className={styles["comments-section"]}>
-      <CreateComment />
-      {comments.comments.map((comment, index) => (
-        <Comment key={index} comment={comment} />
-      ))}
+      <CreateComment postID={postID} />
+      {commentsArr.length >= 0 ? (
+        commentsArr.map((comment) => <Comment key={v4()} comment={comment} />)
+      ) : (
+        <div className={styles["no-comments"]}>Be the first to comment!</div>
+      )}
     </div>
   );
 };
