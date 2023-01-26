26b2aaa678ce3e86c9428d9701a66a75d21ed66d
Baltej Randhawa <baltej.randhawa0@gmail.com>
2023-01-25 19:47:31 -0800

    likes props

--- frontend/src/components/Post/LikesView/Like/Like.js
+++ frontend/src/components/Post/LikesView/Like/Like.js
@@ -1,11 +1,11 @@
 import React from "react";
 import styles from "./Like.module.css";
 
-const Like = ({ user, picURL }) => {
+const Like = ({ username, picURL, userID }) => {
   return (
     <div className={styles["like-holder"]}>
-      <img className={styles["like-img"]} src={picURL} alt="profile" />
-      {user}
+      {/* <img className={styles["like-img"]} src={picURL} alt="profile" /> */}
+      {username}
     </div>
   );
 };
