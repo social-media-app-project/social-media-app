d150a90dc21dd5f4c3e264cb98613ad8e1ad6a27
Baltej Randhawa <baltej.randhawa0@gmail.com>
2023-01-26 19:19:02 -0800

    created user controller

---
+++ api/controllers/UserController.js
@@ -0,0 +1,7 @@
+const { sendResponseOnError } = require("../middleware/validators/util");
+
+exports.getFriends = [];
+exports.postAddFriend = [];
+exports.updateUsername = [];
+exports.updateBio = [];
+exports.deleteFriend = [];
