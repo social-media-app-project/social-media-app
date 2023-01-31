cbaaf1544f05eb5825da113db5426097a2832632
Baltej Randhawa <baltej.randhawa0@gmail.com>
2023-01-30 19:29:48 -0800

    created user routes

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,13 +2,13 @@ const express = require("express");
 const UserController = require("../controllers/UserController");
 const router = express.Router();
 
-router.get("/friend/:username");
+router.get("/friend/:username", UserController.getFriends);
 
-router.post("/friend/:friendId");
+router.post("/friend/:friendId", UserController.postAddFriend);
 
-router.put("/update/username");
-router.put("/update/bio");
+router.put("/update/username", UserController.updateUsername);
+router.put("/update/bio", UserController.updateBio);
 
-router.delete("/delete/:friendId");
+router.delete("/delete/:friendId", UserController.deleteFriend);
 
 module.exports = router;
