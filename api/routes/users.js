f3e43d9ce2475e81aebfd2c2cd971de235ca686f
Baltej Randhawa <baltej.randhawa0@gmail.com>
2023-02-14 15:46:45 -0800

    added add / del friend routes

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,14 +2,17 @@ const express = require("express");
 const UserController = require("../controllers/UserController");
 const router = express.Router();
 
+router.get("/user", UserController.getUser);
 router.get("/friend/:username", UserController.getFriends);
 
-router.post("/username", UserController.checkAvailibility);
-router.post("/friend/:friendId", UserController.postAddFriend);
+router.post("/:username", UserController.checkAvailibility);
+router.post("/friend/:friendId/request", UserController.postSendFriendRequest);
+router.post("/friend/:friendId/accept", UserController.postAcceptFriendRequest);
 
-router.put("/update/username", UserController.updateUsername);
+router.put("/update/username/:username", UserController.updateUsername);
 router.put("/update/bio", UserController.updateBio);
 
 router.delete("/delete/:friendId", UserController.deleteFriend);
+router.delete("/delete/:friendId/request", UserController.deleteFriendRequest);
 
 module.exports = router;
