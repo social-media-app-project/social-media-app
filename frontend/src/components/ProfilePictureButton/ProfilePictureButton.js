8370c5acc87a876509b4c3c2456543fd8361fc9c
Baltej Randhawa <baltej.randhawa0@gmail.com>
2023-02-20 18:30:35 -0800

    added avatar

--- frontend/src/components/ProfilePictureButton/ProfilePictureButton.js
+++ frontend/src/components/ProfilePictureButton/ProfilePictureButton.js
@@ -1,19 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./ProfilePictureButton.module.css";
-import profilePicTest from "../../test-data/test-images/profilePicTest.jpg";
 
 const ProfilePictureButton = (props) => {
-  const { width, height } = props;
-
   return (
     <Link to="/">
       <img
-        src={profilePicTest}
-        alt="person"
+        src={props?.imgUrl}
+        alt="avatar"
         className={styles["profile-pic-link"]}
-        width={width ? width : undefined}
-        height={height ? height : undefined}
       />
     </Link>
   );
