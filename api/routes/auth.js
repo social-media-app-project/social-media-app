5e6eacec876cdc531516c610818acbdbbf00eac9
Baltej Randhawa <baltej.randhawa0@gmail.com>
2023-03-10 09:50:22 -0800

    added google sign in routes

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const passport = require('passport')
 
 const router = express.Router();
 const AuthController = require('../controllers/AuthController');
 
 router.post('/signup', AuthController.jwtSignupPost);
 router.post('/login', AuthController.jwtLoginPost);
+router.get('/login/google', AuthController.googleSignIn)
+router.get('/login/google/callback', AuthController.googleSignInCallback)
 
 module.exports = router;
