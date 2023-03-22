9ebe71434335f1150f5f0164b6d6da95592be555
Baltej Randhawa <baltej.randhawa0@gmail.com>
2023-03-21 19:42:22 -0700

    updated

--- api/models/Comment.js
+++ api/models/Comment.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+mongoose.set('strictQuery', false);
 const { Schema } = mongoose;
 
 const CommentSchema = new Schema({
--- api/models/User.js
+++ api/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+mongoose.set('strictQuery', false);
 
 const { Schema } = mongoose;
 const { DateTime } = require("luxon");
